b33a0cf9844e7d19bac240eb146ba0c5be16405f
Rafaella Bambokian <rafaellabambokian@outlook.com>
2020-05-06 02:12:18 -0400

    Criando labirinto com cubos boxGeometry

--- js/index.js
+++ js/index.js
@@ -3,8 +3,41 @@ const canvasWidth = document.getElementById(id).offsetWidth;
 const canvasHeight = document.getElementById(id).offsetHeight;
 const webGLExists = Detector.webgl ? true : false;
 
-var renderer, scene, camera, controls;
-var cube;
+var renderer, scene, camera, controls, map;
+var LEVEL = 
+[
+  '# # # # # # # # # # # # # # # # # # # # # # # # # # # #',
+  '# . . . . . . . . . . . . # # . . . . . . . . . . . . #',
+  '# . # # # # . # # # # # . # # . # # # # # . # # # # . #',
+  '# o # # # # . # # # # # . # # . # # # # # . # # # # o #',
+  '# . # # # # . # # # # # . # # . # # # # # . # # # # . #',
+  '# . . . . . . . . . . . . . . . . . . . . . . . . . . #',
+  '# . # # # # . # # . # # # # # # # # . # # . # # # # . #',
+  '# . # # # # . # # . # # # # # # # # . # # . # # # # . #',
+  '# . . . . . . # # . . . . # # . . . . # # . . . . . . #',
+  '# # # # # # . # # # # #   # #   # # # # # . # # # # # #',
+  '          # . # # # # #   # #   # # # # # . #          ',
+  '          # . # #         G           # # . #          ',
+  '          # . # #   # # # # # # # #   # # . #          ',
+  '# # # # # # . # #   #             #   # # . # # # # # #',
+  '            .       #             #       .            ',
+  '# # # # # # . # #   #             #   # # . # # # # # #',
+  '          # . # #   # # # # # # # #   # # . #          ',
+  '          # . # #                     # # . #          ',
+  '          # . # #   # # # # # # # #   # # . #          ',
+  '# # # # # # . # #   # # # # # # # #   # # . # # # # # #',
+  '# . . . . . . . . . . . . # # . . . . . . . . . . . . #',
+  '# . # # # # . # # # # # . # # . # # # # # . # # # # . #',
+  '# . # # # # . # # # # # . # # . # # # # # . # # # # . #',
+  '# o . . # # . . . . . . . P   . . . . . . . # # . . o #',
+  '# # # . # # . # # . # # # # # # # # . # # . # # . # # #',
+  '# # # . # # . # # . # # # # # # # # . # # . # # . # # #',
+  '# . . . . . . # # . . . . # # . . . . # # . . . . . . #',
+  '# . # # # # # # # # # # . # # . # # # # # # # # # # . #',
+  '# . # # # # # # # # # # . # # . # # # # # # # # # # . #',
+  '# . . . . . . . . . . . . . . . . . . . . . . . . . . #',
+  '# # # # # # # # # # # # # # # # # # # # # # # # # # # #'
+];
 
 initApp();
 
@@ -12,9 +45,10 @@ function initApp() {
   if(webGLExists === true) {
     createRenderer();
     createScene();
+    createMap(LEVEL);
     createPerspectiveCamera();
     drawAxes(15);
-    drawCube();
+    createWallMaze();
     animateScene();
     
   } else if(webGLExists === false) {
@@ -31,15 +65,15 @@ function createRenderer() {
 
 function createScene() {
   scene = new THREE.Scene();
-  // scene.add(new THREE.AmbientLight(0x888888));
-  // var light = new THREE.SpotLight('white', 0.5);
-  // light.position.set(0, 0, 50);
-  // scene.add(light);
+  scene.add(new THREE.AmbientLight(0x888888));
+  var light = new THREE.SpotLight('white', 0.5);
+  light.position.set(0, 0, 50);
+  scene.add(light);
 }
 
 function createPerspectiveCamera() {
   camera = new THREE.PerspectiveCamera(60, canvasWidth / canvasHeight, 0.1, 100);
-  camera.position.set(-3, 3, 6);
+  camera.position.set(-3, 3, 20);
   camera.lookAt(scene.position);
   controls = new THREE.OrbitControls(camera, renderer.domElement);
   // controls.enablePan = false;
@@ -53,17 +87,53 @@ function drawAxes(length) {
   scene.add(axes);
 }
 
-function drawCube() {
+function createWallMaze() {
   var geometry = new THREE.BoxGeometry(1, 1, 1);
-  var material = new THREE.MeshBasicMaterial({ color: 0x0fffff });
-  cube = new THREE.Mesh(geometry, material);
-  scene.add(cube);
+  var material = new THREE.MeshPhongMaterial({ color: 0x00ffff });
+  var wall = new THREE.Mesh(geometry, material);
+  // wall.isWall = true;
 
-  return cube;
+  return wall;
+}
+
+function createMap(levelDef) {
+  var map = {};
+  map.bottom = -(levelDef.length - 1);
+  map.top = 0;
+  map.left = 0;
+  map.right = 0;
+
+  var x, y;
+  for (var row = 0; row < levelDef.length; row++) {
+    y = -row + (levelDef.length-3)/2;
+    map[y] = {};
+
+    var length = Math.floor(levelDef[row].length / 2);
+    map.right = Math.max(map.right, length);
+
+    for (var column = 0; column < levelDef[row].length; column += 2) {
+      x = Math.floor(column / 2) + (2 - levelDef.length/2);
+      var cell = levelDef[row][column];
+      var object = null;
+
+      if (cell === '#') {
+        object = createWallMaze();
+      }
+
+      if (object !== null) {
+        object.position.set(x, y, 0);
+        map[y][x] = object;
+        scene.add(object);
+      }
+    }
+  }
+  map.centerX = (map.left + map.right) / 2;
+  map.centerY = (map.bottom + map.top) / 2;
+
+  return map;
 }
 
 function animateScene() {
   requestAnimationFrame(animateScene);
-  // cube.rotation.x += 5;
   renderer.render(scene, camera);
 };
